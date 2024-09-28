75b05db774fdb49d27c59f20362685015ebfa600
Frazer <fgerrymok@gmail.com>
2024-09-27 21:12:22 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* Use the rest API to output name, title and bio*/}
+      <h1>This is my website</h1>
+    </>
+  )
+}
+
+export default App
