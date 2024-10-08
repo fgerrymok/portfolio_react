756ffed37510969924ee14a0766ac3c4f94a8be0
Frazer <fgerrymok@gmail.com>
2024-10-07 22:08:20 -0700

    Added copy to clipboard function in hero section. Imported site logo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../App";
 import { toggleNav } from "../utilities/Utilities";
+import logo from "../assets/images/frazer-site-logo.png";
 
 function Header() {
     const [menuActive, setMenuActive] = useContext(Context);
@@ -9,6 +10,7 @@ function Header() {
     return (
         <>
             <header onClick={() => {menuActive ? toggleNav(menuActive, setMenuActive) : null}}>
+                <img src={logo} alt="Site Logo" className="site-logo"/>
                 <button className={menuActive ? "menu-button-fixed" : "menu-button"} onClick={() => {toggleNav(menuActive, setMenuActive)}}>
                     <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z" fillRule="nonzero"/></svg>
                 </button>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,6 +9,8 @@ function Homepage() {
     const [homepageData, setHomepageData] = useState({});
     const [homepageLoaded, setHomepageLoaded] = useState(false);
     const [menuActive, setMenuActive] = useContext(Context);
+    const email = "fgerrymok@gmail.com"; // change to an ACF field later
+    const [emailCopied, setEmailCopied] = useState(false);
 
     useEffect(() => {
         async function fetchHomepageData() {
@@ -21,6 +23,14 @@ function Homepage() {
         }
         fetchHomepageData();
     }, [homepagePath])
+
+    function copyToClipboard() {
+        navigator.clipboard.writeText(email)
+        setEmailCopied(true);
+        setTimeout(() => {
+            setEmailCopied(false)
+        }, 2000)
+    }
 
     return (
         <>
@@ -35,11 +45,11 @@ function Homepage() {
                         <Link to="https://www.linkedin.com/in/frazermok/" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 8c0 .557-.447 1.008-1 1.008s-1-.45-1-1.008c0-.557.447-1.008 1-1.008s1 .452 1 1.008zm0 2h-2v6h2v-6zm3 0h-2v6h2v-2.861c0-1.722 2.002-1.881 2.002 0v2.861h1.998v-3.359c0-3.284-3.128-3.164-4-1.548v-1.093z"/></svg>
                         </Link>
-                        <Link>
+                        <button className="email-button" onClick={copyToClipboard}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.02c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 12.55l-5.992-4.57h11.983l-5.991 4.57zm0 1.288l-6-4.629v6.771h12v-6.771l-6 4.629z"/></svg>
-                        </Link>
+                        </button>
+                        <div className={emailCopied ? "copied-email-msg-visible" : "copied-email-msg-invisible"}>Email Copied To Clipboard!</div>
                     </div>
-                    
                     <div className="dev-info">
                         <h1><span className="code-design">/</span>{homepageData.acf.name}</h1>
                         <h2 className="wavy-text">
